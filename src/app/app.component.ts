4aba71aeee536ae1e264ae265a9c623d303056a0
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2018-09-24 21:27:14 -0400

    Add mutation to create a new user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ export class AppComponent {
   private apiURL = 'https://api.graph.cool/simple/v1/cjil4jc9c2xt90191zd4p3ngy';
 
   constructor(private http: HttpClient) {
-    this.allUsers();
+    // this.createUser();
+    // this.allUsers();
   }
 
   allUsers(): void {
@@ -24,10 +25,32 @@ export class AppComponent {
             name
             email
           }
-      }`
+        }`
     };
 
     this.http.post(this.apiURL, body)
       .subscribe(res => console.log(res));
+  }
+
+  createUser(): void {
+
+    const body = {
+      query: `
+        mutation CreateNewUser($name: String!, $email: String!, $password: String!) {
+          createUser(name: $name, email: $email, password: $password) {
+            id
+            name
+            email
+          }
+        }`,
+        variables: {
+          name: 'Black Panther',
+          email: 'panther@avengers.com',
+          password: '123456'
+        }
+    };
+
+    this.http.post(this.apiURL, body)
+      .subscribe(res => console.log('Mutation', res));
   }
 }
